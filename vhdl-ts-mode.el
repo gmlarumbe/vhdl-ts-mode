5836c668eea4e3e208bc3149287dfc1971885ce7
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2023-09-12 20:52:19 +0200

    Update version to 0.1.0

--- vhdl-ts-mode.el
+++ vhdl-ts-mode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
 ;; URL: https://github.com/gmlarumbe/vhdl-ts-mode
-;; Version: 0.0.0
+;; Version: 0.1.0
 ;; Keywords: VHDL, IDE, Tools
 ;; Package-Requires: ((emacs "29.1"))
 
