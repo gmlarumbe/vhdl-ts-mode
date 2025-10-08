fc9e174c1f9fccaf6c9bfbc2710162c87778aad2
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2025-10-08 22:52:35 +0200

    Update header commentary section

--- vhdl-ts-mode.el
+++ vhdl-ts-mode.el
@@ -24,7 +24,7 @@
 ;;; Commentary:
 
 ;; Major mode to navigate and edit VHDL files with tree-sitter.
-;;
+
 ;; Provides tree-sitter based implementations for the following features:
 ;; - Syntax highlighting
 ;; - Indentation
@@ -32,13 +32,12 @@
 ;; - `which-func'
 ;; - Navigation functions
 ;; - Completion at point
-;;
-;;
+
 ;; Contributions:
 ;;   This major mode is still under active development!
 ;;   Check contributing guidelines:
 ;;     - https://github.com/gmlarumbe/vhdl-ts-mode#contributing
-;;
+
 ;;   For some highlight queries examples, check the link below:
 ;;    - https://github.com/alemuller/tree-sitter-vhdl/blob/main/queries/highlights.scm
 
